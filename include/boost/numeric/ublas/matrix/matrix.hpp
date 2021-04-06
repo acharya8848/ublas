b0dcd3f80ab218a8950dda45540e39145f394fe4
Anubhav Acharya <44275326+acharya8848@users.noreply.github.com>
2021-04-06 00:40:32 -0400

    Create matrix.hpp

---
+++ include/boost/numeric/ublas/matrix/matrix.hpp
@@ -0,0 +1,19 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include <vector>
+#include <tuple>
+//#include <>
+
+namespace Matrix {
+
+	class Matrix {
+	  Matrix zeros(int x, int y)
+	  {
+	    
+	  }
+	};
+
+}
+
+#endif
